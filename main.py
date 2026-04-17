ad3e1364ab6ded92055af419bd594c84b7e3a85e
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 00:45:34 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,5 @@
+haroratlar = [25, 30, 28, 22, 20]
+shaharlar = ["Toshkent", "Samarqand", "Buxoro", "Fargona", "Namangan"]
+
+for i in range(len(haroratlar)):
+    print(f"Shahar: {shaharlar[i]} {haroratlar[i]}°C")
